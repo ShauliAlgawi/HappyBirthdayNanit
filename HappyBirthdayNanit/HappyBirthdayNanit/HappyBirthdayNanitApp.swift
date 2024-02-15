77e536b0dcc9e993a9d4b989a4e99dce91c6bf54
ShauliAlgawi <160117907+ShauliAlgawi@users.noreply.github.com>
2024-02-15 01:35:00 -0800

    FirstPush
    First Push

---
+++ HappyBirthdayNanit/HappyBirthdayNanit/HappyBirthdayNanitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdayNanitApp.swift
+//  HappyBirthdayNanit
+//
+//  Created by Shauli Algawi on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdayNanitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
