783392b5811fb681d73c3662116b9f02cdba3061
ShauliAlgawi <160117907+ShauliAlgawi@users.noreply.github.com>
2024-02-15 02:25:35 -0800

    FirstCommit
    First

---
+++ HappyBirthday/HappyBirthday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyBirthday
+//
+//  Created by Shauli Algawi on 15/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
